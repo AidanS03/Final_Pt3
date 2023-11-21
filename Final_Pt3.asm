55b3cf7bb9fa234cbf43bd83fcadd9c46a8c6879
Astoner <aidanstoner1@gmail.com>
2023-11-20 21:18:36 -0500

    no worky

--- Final_Pt3.asm
+++ Final_Pt3.asm
@@ -1,7 +1,124 @@
 _main:
-;Final_Pt3.c,1 :: 		void main() {
-;Final_Pt3.c,3 :: 		}
+;Final_Pt3.c,17 :: 		void main() {
+;Final_Pt3.c,18 :: 		initGPIO();
+BL	_initGPIO+0
+;Final_Pt3.c,19 :: 		initTIM1();
+BL	_initTIM1+0
+;Final_Pt3.c,20 :: 		for(;;){
+L_main0:
+;Final_Pt3.c,21 :: 		if(TIM1_SR.UIF == 1){ //check if counter reached target value
+MOVW	R0, #lo_addr(TIM1_SR+0)
+MOVT	R0, #hi_addr(TIM1_SR+0)
+_LX	[R0, ByteOffset(TIM1_SR+0)]
+CMP	R0, #0
+IT	EQ
+BEQ	L_main3
+;Final_Pt3.c,22 :: 		TIM1_SR.UIF = 0; //clear the check bit
+MOVS	R1, #0
+SXTB	R1, R1
+MOVW	R0, #lo_addr(TIM1_SR+0)
+MOVT	R0, #hi_addr(TIM1_SR+0)
+_SX	[R0, ByteOffset(TIM1_SR+0)]
+;Final_Pt3.c,23 :: 		GPIOE_ODR = ~ GPIOE_ODR; //flips PE8
+MOVW	R0, #lo_addr(GPIOE_ODR+0)
+MOVT	R0, #hi_addr(GPIOE_ODR+0)
+LDR	R0, [R0, #0]
+MVN	R1, R0
+MOVW	R0, #lo_addr(GPIOE_ODR+0)
+MOVT	R0, #hi_addr(GPIOE_ODR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,24 :: 		}
+L_main3:
+;Final_Pt3.c,25 :: 		}
+IT	AL
+BAL	L_main0
+;Final_Pt3.c,26 :: 		}
 L_end_main:
 L__main_end_loop:
 B	L__main_end_loop
 ; end of _main
+_initTIM1:
+;Final_Pt3.c,28 :: 		void initTIM1(){
+;Final_Pt3.c,29 :: 		RCC_APB2ENR |= 1 << 11; //enable clock for TIM1
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+LDR	R0, [R0, #0]
+ORR	R1, R0, #2048
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,30 :: 		TIM1_CR1 = 0; //clear control register for initialization
+MOVS	R1, #0
+MOVW	R0, #lo_addr(TIM1_CR1+0)
+MOVT	R0, #hi_addr(TIM1_CR1+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,31 :: 		TIM1_PSC = 7999; //1 second if counting to 5000
+MOVW	R1, #7999
+MOVW	R0, #lo_addr(TIM1_PSC+0)
+MOVT	R0, #hi_addr(TIM1_PSC+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,33 :: 		TIM1_ARR = 9000;  //target value for the counter
+MOVW	R1, #9000
+MOVW	R0, #lo_addr(TIM1_ARR+0)
+MOVT	R0, #hi_addr(TIM1_ARR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,34 :: 		TIM1_CR1 = 1;     //enable timer
+MOVS	R1, #1
+MOVW	R0, #lo_addr(TIM1_CR1+0)
+MOVT	R0, #hi_addr(TIM1_CR1+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,35 :: 		}
+L_end_initTIM1:
+BX	LR
+; end of _initTIM1
+_initGPIO:
+;Final_Pt3.c,37 :: 		void initGPIO(){  //starts the clocks for GPIO
+;Final_Pt3.c,38 :: 		RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+LDR	R0, [R0, #0]
+ORR	R1, R0, #4
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,39 :: 		RCC_APB2ENR |= 1 << 3;  //enables clock for PortB
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+LDR	R0, [R0, #0]
+ORR	R1, R0, #8
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,40 :: 		RCC_APB2ENR |= 1 << 4;  //enables clock for PortC
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+LDR	R0, [R0, #0]
+ORR	R1, R0, #16
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,41 :: 		RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+LDR	R0, [R0, #0]
+ORR	R1, R0, #32
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,42 :: 		RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+LDR	R0, [R0, #0]
+ORR	R1, R0, #64
+MOVW	R0, #lo_addr(RCC_APB2ENR+0)
+MOVT	R0, #hi_addr(RCC_APB2ENR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,44 :: 		GPIOE_CRH = 0x33333333; // sets PE8 as an output
+MOV	R1, #858993459
+MOVW	R0, #lo_addr(GPIOE_CRH+0)
+MOVT	R0, #hi_addr(GPIOE_CRH+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,45 :: 		}
+L_end_initGPIO:
+BX	LR
+; end of _initGPIO
--- Final_Pt3.c
+++ Final_Pt3.c
@@ -9,9 +9,37 @@
 
 //******************************************************************************
 //Functions:
+void initTIM1();
+void initGPIO();
 
 //******************************************************************************
 //Main:
 void main() {
+     initGPIO();
+     initTIM1();
+     for(;;){
+          if(TIM1_SR.UIF == 1){ //check if counter reached target value
+               TIM1_SR.UIF = 0; //clear the check bit
+               GPIOE_ODR = ~ GPIOE_ODR; //flips PE8
+          }
+     }
+}
 
+void initTIM1(){
+     RCC_APB2ENR |= 1 << 11; //enable clock for TIM1
+     TIM1_CR1 = 0; //clear control register for initialization
+     TIM1_PSC = 7999; //1 second if counting to 5000
+                       //5000 = 72,000,000/(14399 + 1)
+     TIM1_ARR = 9000;  //target value for the counter
+     TIM1_CR1 = 1;     //enable timer
+}
+
+void initGPIO(){  //starts the clocks for GPIO
+     RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
+     RCC_APB2ENR |= 1 << 3;  //enables clock for PortB
+     RCC_APB2ENR |= 1 << 4;  //enables clock for PortC
+     RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
+     RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
+     
+     GPIOE_CRH = 0x33333333; // sets PE8 as an output
 }
