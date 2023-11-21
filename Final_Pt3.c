c4b7687ddd223ae1960bad794a4e9072bac08779
Astoner <aidanstoner1@gmail.com>
2023-11-20 20:40:52 -0500

    added header and basic blocks

--- Final_Pt3.c
+++ Final_Pt3.c
@@ -1,3 +1,17 @@
+//Name: Aidan Stoner, Shaunessy Reynolds, Erin Cardino, Marc Santacapita
+//Date: 12/4/23
+//Course: ELEC-3371
+//Description: Use interupts to flash an LED when TIM1 reaches one second, update 
+//             TIM3 when the analog potentiometer changes, rewrite the joystick fucntion
+//             using interupts, and activate a buzzer using an interupt
+//******************************************************************************
+//Global Variables:
+
+//******************************************************************************
+//Functions:
+
+//******************************************************************************
+//Main:
 void main() {
 
 }
