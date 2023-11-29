7356e00f875085651a7354a5c6b3cf0f2b5ea742
Astoner <aidanstoner1@gmail.com>
2023-11-29 14:53:53 -0500

    obj 2 pretty much done just need to add in the count to USART

--- Final_Pt3.asm
+++ Final_Pt3.asm
@@ -1,12 +1,12 @@
 _TIM1_ISR:
-;Final_Pt3.c,17 :: 		void TIM1_ISR() iv IVT_INT_TIM1_UP {
-;Final_Pt3.c,18 :: 		TIM1_SR.UIF = 0; //clear the check bit
+;Final_Pt3.c,20 :: 		void TIM1_ISR() iv IVT_INT_TIM1_UP {
+;Final_Pt3.c,21 :: 		TIM1_SR.UIF = 0; //clear the check bit
 MOVS	R1, #0
 SXTB	R1, R1
 MOVW	R0, #lo_addr(TIM1_SR+0)
 MOVT	R0, #hi_addr(TIM1_SR+0)
 _SX	[R0, ByteOffset(TIM1_SR+0)]
-;Final_Pt3.c,19 :: 		GPIOA_ODR.B0 = ~GPIOA_ODR.B0; //flips PE8
+;Final_Pt3.c,22 :: 		GPIOA_ODR.B0 = ~GPIOA_ODR.B0; //flips PE8
 MOVW	R0, #lo_addr(GPIOA_ODR+0)
 MOVT	R0, #hi_addr(GPIOA_ODR+0)
 _LX	[R0, ByteOffset(GPIOA_ODR+0)]
@@ -15,29 +15,105 @@ UXTB	R1, R1
 MOVW	R0, #lo_addr(GPIOA_ODR+0)
 MOVT	R0, #hi_addr(GPIOA_ODR+0)
 _SX	[R0, ByteOffset(GPIOA_ODR+0)]
-;Final_Pt3.c,20 :: 		}
+;Final_Pt3.c,23 :: 		TIM1count++;
+MOVW	R1, #lo_addr(_TIM1count+0)
+MOVT	R1, #hi_addr(_TIM1count+0)
+LDRSH	R0, [R1, #0]
+ADDS	R0, R0, #1
+STRH	R0, [R1, #0]
+;Final_Pt3.c,24 :: 		}
 L_end_TIM1_ISR:
 BX	LR
 ; end of _TIM1_ISR
 _main:
-;Final_Pt3.c,26 :: 		void main() {
-;Final_Pt3.c,27 :: 		initGPIO();
+;Final_Pt3.c,30 :: 		void main() {
+;Final_Pt3.c,31 :: 		initGPIO();
 BL	_initGPIO+0
-;Final_Pt3.c,28 :: 		initTIM1();
+;Final_Pt3.c,32 :: 		initTIM1();
 BL	_initTIM1+0
-;Final_Pt3.c,29 :: 		for(;;){
+;Final_Pt3.c,33 :: 		TIM1count = -1;
+MOVW	R1, #65535
+SXTH	R1, R1
+MOVW	R0, #lo_addr(_TIM1count+0)
+MOVT	R0, #hi_addr(_TIM1count+0)
+STRH	R1, [R0, #0]
+;Final_Pt3.c,34 :: 		for(;;){
 L_main0:
-;Final_Pt3.c,31 :: 		}
+;Final_Pt3.c,35 :: 		if(TIM1count >= 100){TIM1count = 0;}
+MOVW	R0, #lo_addr(_TIM1count+0)
+MOVT	R0, #hi_addr(_TIM1count+0)
+LDRSH	R0, [R0, #0]
+CMP	R0, #100
+IT	LT
+BLT	L_main3
+MOVS	R1, #0
+SXTH	R1, R1
+MOVW	R0, #lo_addr(_TIM1count+0)
+MOVT	R0, #hi_addr(_TIM1count+0)
+STRH	R1, [R0, #0]
+L_main3:
+;Final_Pt3.c,36 :: 		find7segVal(TIM1count);
+MOVW	R0, #lo_addr(_TIM1count+0)
+MOVT	R0, #hi_addr(_TIM1count+0)
+LDRSH	R0, [R0, #0]
+BL	_find7segVal+0
+;Final_Pt3.c,37 :: 		GPIOD_ODR = leftNum[leftIDX];
+MOVW	R0, #lo_addr(_leftIDX+0)
+MOVT	R0, #hi_addr(_leftIDX+0)
+LDRSH	R0, [R0, #0]
+LSLS	R1, R0, #1
+MOVW	R0, #lo_addr(_leftNum+0)
+MOVT	R0, #hi_addr(_leftNum+0)
+ADDS	R0, R0, R1
+LDRSH	R1, [R0, #0]
+MOVW	R0, #lo_addr(GPIOD_ODR+0)
+MOVT	R0, #hi_addr(GPIOD_ODR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,38 :: 		delay_ms(1);
+MOVW	R7, #11999
+MOVT	R7, #0
+NOP
+NOP
+L_main4:
+SUBS	R7, R7, #1
+BNE	L_main4
+NOP
+NOP
+NOP
+;Final_Pt3.c,39 :: 		GPIOD_ODR = rightNum[rightIDX];
+MOVW	R0, #lo_addr(_rightIDX+0)
+MOVT	R0, #hi_addr(_rightIDX+0)
+LDRSH	R0, [R0, #0]
+LSLS	R1, R0, #1
+MOVW	R0, #lo_addr(_rightNum+0)
+MOVT	R0, #hi_addr(_rightNum+0)
+ADDS	R0, R0, R1
+LDRSH	R1, [R0, #0]
+MOVW	R0, #lo_addr(GPIOD_ODR+0)
+MOVT	R0, #hi_addr(GPIOD_ODR+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,40 :: 		delay_ms(1);
+MOVW	R7, #11999
+MOVT	R7, #0
+NOP
+NOP
+L_main6:
+SUBS	R7, R7, #1
+BNE	L_main6
+NOP
+NOP
+NOP
+;Final_Pt3.c,41 :: 		}
 IT	AL
 BAL	L_main0
-;Final_Pt3.c,32 :: 		}
+;Final_Pt3.c,42 :: 		}
 L_end_main:
 L__main_end_loop:
 B	L__main_end_loop
 ; end of _main
 _initTIM1:
-;Final_Pt3.c,34 :: 		void initTIM1(){
-;Final_Pt3.c,35 :: 		RCC_APB2ENR |= 1 << 11; //enable clock for TIM1
+;Final_Pt3.c,44 :: 		void initTIM1(){
+;Final_Pt3.c,45 :: 		RCC_APB2ENR |= 1 << 11; //enable clock for TIM1
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 LDR	R0, [R0, #0]
@@ -45,46 +121,45 @@ ORR	R1, R0, #2048
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,36 :: 		TIM1_CR1 = 0x0000; //clear control register for initialization
+;Final_Pt3.c,46 :: 		TIM1_CR1 = 0x0000; //clear control register for initialization
 MOVS	R1, #0
 MOVW	R0, #lo_addr(TIM1_CR1+0)
 MOVT	R0, #hi_addr(TIM1_CR1+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,37 :: 		TIM1_PSC = 14399; //1 second if counting to 5000
-MOVW	R1, #14399
+;Final_Pt3.c,47 :: 		TIM1_PSC = 7999; //1 second if counting to 9000
+MOVW	R1, #7999
 MOVW	R0, #lo_addr(TIM1_PSC+0)
 MOVT	R0, #hi_addr(TIM1_PSC+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,39 :: 		TIM1_ARR = 5000;  //target value for the counter
-MOVW	R1, #5000
+;Final_Pt3.c,49 :: 		TIM1_ARR = 9000;  //target value for the counter
+MOVW	R1, #9000
 MOVW	R0, #lo_addr(TIM1_ARR+0)
 MOVT	R0, #hi_addr(TIM1_ARR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,40 :: 		NVIC_ISER0 |= (long)(1 << (long)25); //Enable break interrupt for TIM1
-MOVW	R0, #lo_addr(NVIC_ISER0+0)
-MOVT	R0, #hi_addr(NVIC_ISER0+0)
-LDR	R1, [R0, #0]
-MOVW	R0, #lo_addr(NVIC_ISER0+0)
-MOVT	R0, #hi_addr(NVIC_ISER0+0)
-STR	R1, [R0, #0]
-;Final_Pt3.c,41 :: 		TIM1_DIER.UIE = 1; //enable update interrupts for our timer
-MOVS	R1, #1
-SXTB	R1, R1
+;Final_Pt3.c,50 :: 		NVIC_ISER0.B25 = 1; //Enable update interrupt for TIM1
+MOVS	R2, #1
+SXTB	R2, R2
+MOVW	R1, #lo_addr(NVIC_ISER0+0)
+MOVT	R1, #hi_addr(NVIC_ISER0+0)
+LDR	R0, [R1, #0]
+BFI	R0, R2, #25, #1
+STR	R0, [R1, #0]
+;Final_Pt3.c,51 :: 		TIM1_DIER.UIE = 1; //enable update interrupts for our timer
 MOVW	R0, #lo_addr(TIM1_DIER+0)
 MOVT	R0, #hi_addr(TIM1_DIER+0)
 _SX	[R0, ByteOffset(TIM1_DIER+0)]
-;Final_Pt3.c,42 :: 		TIM1_CR1 = 0x0001;     //enable timer
+;Final_Pt3.c,52 :: 		TIM1_CR1 = 0x0001;     //enable timer
 MOVS	R1, #1
 MOVW	R0, #lo_addr(TIM1_CR1+0)
 MOVT	R0, #hi_addr(TIM1_CR1+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,43 :: 		}
+;Final_Pt3.c,53 :: 		}
 L_end_initTIM1:
 BX	LR
 ; end of _initTIM1
 _initGPIO:
-;Final_Pt3.c,45 :: 		void initGPIO(){  //starts the clocks for GPIO
-;Final_Pt3.c,46 :: 		RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
+;Final_Pt3.c,55 :: 		void initGPIO(){  //starts the clocks for GPIO
+;Final_Pt3.c,56 :: 		RCC_APB2ENR |= 1 << 2;  //enables clock for PortA
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 LDR	R0, [R0, #0]
@@ -92,7 +167,7 @@ ORR	R1, R0, #4
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,47 :: 		RCC_APB2ENR |= 1 << 3;  //enables clock for PortB
+;Final_Pt3.c,57 :: 		RCC_APB2ENR |= 1 << 3;  //enables clock for PortB
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 LDR	R0, [R0, #0]
@@ -100,7 +175,7 @@ ORR	R1, R0, #8
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,48 :: 		RCC_APB2ENR |= 1 << 4;  //enables clock for PortC
+;Final_Pt3.c,58 :: 		RCC_APB2ENR |= 1 << 4;  //enables clock for PortC
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 LDR	R0, [R0, #0]
@@ -108,7 +183,7 @@ ORR	R1, R0, #16
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,49 :: 		RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
+;Final_Pt3.c,59 :: 		RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 LDR	R0, [R0, #0]
@@ -116,7 +191,7 @@ ORR	R1, R0, #32
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,50 :: 		RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
+;Final_Pt3.c,60 :: 		RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 LDR	R0, [R0, #0]
@@ -124,12 +199,44 @@ ORR	R1, R0, #64
 MOVW	R0, #lo_addr(RCC_APB2ENR+0)
 MOVT	R0, #hi_addr(RCC_APB2ENR+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,52 :: 		GPIOA_CRL = 0x3; // sets PortA/L as an output
+;Final_Pt3.c,62 :: 		GPIOA_CRL = 0x3; // sets PA0 as an output
 MOVS	R1, #3
 MOVW	R0, #lo_addr(GPIOA_CRL+0)
 MOVT	R0, #hi_addr(GPIOA_CRL+0)
 STR	R1, [R0, #0]
-;Final_Pt3.c,53 :: 		}
+;Final_Pt3.c,63 :: 		GPIOD_CRH = 0x33333333; //set PortE/H as an output
+MOV	R1, #858993459
+MOVW	R0, #lo_addr(GPIOD_CRH+0)
+MOVT	R0, #hi_addr(GPIOD_CRH+0)
+STR	R1, [R0, #0]
+;Final_Pt3.c,64 :: 		}
 L_end_initGPIO:
 BX	LR
 ; end of _initGPIO
+_find7segVal:
+;Final_Pt3.c,66 :: 		void find7segVal(int sec){
+; sec start address is: 0 (R0)
+; sec end address is: 0 (R0)
+; sec start address is: 0 (R0)
+;Final_Pt3.c,67 :: 		leftIDX = (sec/10);
+MOVS	R1, #10
+SXTH	R1, R1
+SDIV	R2, R0, R1
+SXTH	R2, R2
+MOVW	R1, #lo_addr(_leftIDX+0)
+MOVT	R1, #hi_addr(_leftIDX+0)
+STRH	R2, [R1, #0]
+;Final_Pt3.c,68 :: 		rightIDX = sec - (leftIDX*10);
+MOVS	R1, #10
+SXTH	R1, R1
+MULS	R1, R2, R1
+SXTH	R1, R1
+SUB	R2, R0, R1
+; sec end address is: 0 (R0)
+MOVW	R1, #lo_addr(_rightIDX+0)
+MOVT	R1, #hi_addr(_rightIDX+0)
+STRH	R2, [R1, #0]
+;Final_Pt3.c,69 :: 		}
+L_end_find7segVal:
+BX	LR
+; end of _find7segVal
--- Final_Pt3.c
+++ Final_Pt3.c
@@ -6,17 +6,21 @@
 //             using interupts, and activate a buzzer using an interupt
 //******************************************************************************
 //Global Variables:
-
+int TIM1count;
+int leftNum[10] = {0xA000, 0xA100, 0xA400, 0xA500, 0xB000, 0xB100, 0xB400, 0xB500, 0xE000, 0xE100};
+int rightNum[10] = {0xA800, 0xA900, 0xAC00, 0xAD00, 0xB800, 0xB900, 0xBC00, 0xBD00, 0xE800, 0xE900}; //Variables for 7-seg display initially set to 0
+int leftIDX, rightIDX;
 //******************************************************************************
 //Functions:
 void initTIM1();
 void initGPIO();
-
+void find7segVal(int sec);
 //******************************************************************************
 //Interrupt Routines:
 void TIM1_ISR() iv IVT_INT_TIM1_UP {
      TIM1_SR.UIF = 0; //clear the check bit
      GPIOA_ODR.B0 = ~GPIOA_ODR.B0; //flips PE8
+     TIM1count++;
 }
 
 
@@ -26,18 +30,24 @@ void TIM1_ISR() iv IVT_INT_TIM1_UP {
 void main() {
      initGPIO();
      initTIM1();
+     TIM1count = -1;
      for(;;){
-
+          if(TIM1count >= 100){TIM1count = 0;}
+          find7segVal(TIM1count);
+          GPIOD_ODR = leftNum[leftIDX];
+          delay_ms(1);
+          GPIOD_ODR = rightNum[rightIDX];
+          delay_ms(1);
      }
 }
 
 void initTIM1(){
      RCC_APB2ENR |= 1 << 11; //enable clock for TIM1
      TIM1_CR1 = 0x0000; //clear control register for initialization
-     TIM1_PSC = 14399; //1 second if counting to 5000
-                       //5000 = 72,000,000/(14399 + 1)
-     TIM1_ARR = 5000;  //target value for the counter
-     NVIC_ISER0.B25 = 1; //Enable break interrupt for TIM1
+     TIM1_PSC = 7999; //1 second if counting to 9000
+                       //9000 = 72,000,000/(7999 + 1)
+     TIM1_ARR = 9000;  //target value for the counter
+     NVIC_ISER0.B25 = 1; //Enable update interrupt for TIM1
      TIM1_DIER.UIE = 1; //enable update interrupts for our timer
      TIM1_CR1 = 0x0001;     //enable timer
 }
@@ -49,5 +59,11 @@ void initGPIO(){  //starts the clocks for GPIO
      RCC_APB2ENR |= 1 << 5;  //enables clock for PortD
      RCC_APB2ENR |= 1 << 6;  //enables clock for PortE
      
-     GPIOA_CRL = 0x3; // sets PortA/L as an output
+     GPIOA_CRL = 0x3; // sets PA0 as an output
+     GPIOD_CRH = 0x33333333; //set PortE/H as an output
+}
+
+void find7segVal(int sec){
+     leftIDX = (sec/10);
+     rightIDX = sec - (leftIDX*10);
 }
